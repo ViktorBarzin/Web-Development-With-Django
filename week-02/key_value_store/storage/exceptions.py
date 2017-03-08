bf49101431480a3ab9c68170cabcb734eacb5d65
ViktorBarzin <vbarzin@gmail.com>
2017-03-08 17:25:54 +0200

    reworked key-value store with postgre

---
+++ week-02/key_value_store/storage/exceptions.py
@@ -0,0 +1,2 @@
+class UserDoestNotExistException(Exception):
+    pass
---
+++ week-02/key_value_store/storage/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4().hex)
+
+
+class KeyValue(models.Model):
+    key = models.CharField(max_length=255, null=True, blank=True)
+    value = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(User, related_name='key_values')
+
