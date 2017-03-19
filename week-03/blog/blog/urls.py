9e497608f1521e1cb7342ec41468c8d487664a49
ViktorBarzin <vbarzin@gmail.com>
2017-03-19 18:43:59 +0200

    added form template inheritance and django forms

--- week-03/blog/blog/urls.py
+++ week-03/blog/blog/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from lobby.views import index_view, create_blogpost_view, blogpost_info_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index_view, name='index'),
+    url(r'^create-post/$', create_blogpost_view),
+    url(r'^blog/(?P<blog_id>[0-9]+)/$', blogpost_info_view, name='blog_info')
 ]
--- week-03/blog/lobby/models.py
+++ week-03/blog/lobby/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class CreatedAtModelMixin(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        abstract = True
+
+
+class BlogPost(CreatedAtModelMixin, models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    updated = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    blogpost = models.ManyToManyField(BlogPost, related_name='tags')
+
+
+class Comment(CreatedAtModelMixin, models.Model):
+    author_email = models.CharField(max_length=255)
+    content = models.TextField()
+    blogpost = models.ForeignKey(BlogPost, related_name='comments')
+
+
+
