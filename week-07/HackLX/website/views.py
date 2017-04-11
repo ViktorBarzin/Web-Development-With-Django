b780920ffc92bd3cea91bfe5cff11e460ef8ba31
ViktorBarzin <vbarzin@gmail.com>
2017-04-11 21:44:53 +0300

    adding week 7

---
+++ week-07/HackLX/website/views.py
@@ -0,0 +1,89 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse_lazy
+from django.db.models import Q
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.views.generic.edit import FormView, UpdateView, CreateView, DeleteView
+from django.views.generic.list import ListView
+from .mixins import LoginRequiredMixin, UserCanUpdateOrDeleteOfferMixin
+from .models import Offer, Category
+from .forms import CreateOfferModelForm, RegisterModelForm, UpdateOfferModelForm
+
+
+
+class RegisterView(FormView):
+
+    template_name = 'registration/registration.html'
+    form_class = RegisterModelForm
+    success_url = reverse_lazy('website:login')
+
+    def form_valid(self, form):
+        User.objects.create_user(username=form.data['username'], password=form.data['password'])
+        return super().form_valid(form)
+
+class CreateOfferView(LoginRequiredMixin, CreateView):
+    model = Offer
+    template_name = 'website/add_offer.html'
+    form_class = CreateOfferModelForm
+    success_url = reverse_lazy('website:index')
+
+
+    def form_valid(self, form):
+        # offer = form.save(commit=False)
+        # offer.author = self.request.user
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class UpdateOfferView(LoginRequiredMixin, UserCanUpdateOrDeleteOfferMixin, UpdateView):
+    model = Offer
+    form_class = UpdateOfferModelForm
+    template_name = 'website/add_offer.html'
+    success_url = reverse_lazy('website:index')
+
+
+class DeleteOfferView(LoginRequiredMixin, UserCanUpdateOrDeleteOfferMixin, DeleteView):
+    model = Offer
+    template_name = 'website/index.html'
+    success_url = reverse_lazy('website:index')
+
+
+class OfferListView(ListView):
+    model = Offer
+    template_name = 'website/index.html'
+    paginate_by = 2
+    queryset = Offer.objects.order_by('created_at').reverse().filter(choices='approved')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['object_list'] = context['object_list'].select_related('author', 'category').all()
+        return context
+
+
+class PendingOffersView(ListView):
+    model = Offer
+    template_name = 'website/index.html'
+    queryset = Offer.objects.filter(choices='pending')
+
+
+class ApprovedAndRejectedOffers(ListView):
+    model = Offer
+    template_name = 'website/index.html'
+    queryset = Offer.objects.filter(Q(choices='approved') | Q(choices='rejected'))
+
+
+def get_statistics(request):
+    # TODO: Move logic in services
+    categories = Category.objects.all()
+    categories_result = {}
+
+    for category in categories:
+        categories_result[category.name] = category.offer_set.count()
+
+
+    # TODO: Add more statistics information
+
+    return render(request, 'website/statistics.html', locals())
+
