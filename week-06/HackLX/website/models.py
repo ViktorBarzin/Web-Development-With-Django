d1b87f5cf6004361abae200bcd5df71b45e54012
ViktorBarzin <vbarzin@gmail.com>
2017-04-14 22:22:22 +0300

    adding some models

--- week-06/HackLX/website/models.py
+++ week-06/HackLX/website/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __repr__(self):
         return self.name
