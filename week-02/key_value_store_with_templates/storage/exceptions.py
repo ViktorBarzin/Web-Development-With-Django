e761375d5e85639759cd16c0a602c39d1248f48e
ViktorBarzin <vbarzin@gmail.com>
2017-04-14 21:08:06 +0300

    nz

--- week-02/key_value_store_with_templates/storage/exceptions.py
+++
@@ -1,2 +0,0 @@
-class UserDoestNotExistException(Exception):
-    pass
--- week-05/blog/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .views import register_view, login_view
-
-
-urlpatterns = [
-        url(r'register/$', register_view, name='register'),
-        url('login/$', login_view, name='login')
-]
